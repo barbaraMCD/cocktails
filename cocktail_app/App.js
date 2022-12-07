5e956c7aacd0ebbea8fd69313ea281a2b763cb3e
barbaraMCD <102307204+barbaraMCD@users.noreply.github.com>
2022-12-07 16:24:22 +0100

    redux and persist

--- cocktail_app/App.js
+++ cocktail_app/App.js
@@ -1,13 +1,19 @@
 import * as React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
-
+import {PersistGate} from 'redux-persist/integration/react';
 import AppNavigator from './src/components/AppNavigator';
+import {Provider} from 'react-redux';
+import {store, persistor} from './src/store/store';
 
 const App = () => {
   return (
-    <NavigationContainer>
-      <AppNavigator />
-    </NavigationContainer>
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
+        <NavigationContainer>
+          <AppNavigator />
+        </NavigationContainer>
+      </PersistGate>
+    </Provider>
   );
 };
 
