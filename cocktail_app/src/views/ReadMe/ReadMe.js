9cb30fde774bab5d7f9603f07074fe8764c20354
buldOo <killian.gay28@gmail.com>
2022-12-08 16:08:46 +0100

    read me content

--- cocktail_app/src/views/ReadMe/ReadMe.js
+++ cocktail_app/src/views/ReadMe/ReadMe.js
@@ -2,6 +2,7 @@ import React from "react"
 import {Image, Text, TouchableHighlight, View} from 'react-native'
 import { SafeAreaView } from "react-native"
 import styles from './ReadMe.style'
+import react from '../../assets/images/react-native-logo.png'
 
 const ReadMe = () => {
     return (
@@ -9,12 +10,15 @@ const ReadMe = () => {
             <Text style={styles.title}>Read me</Text>
             <View style={styles.sectionContainer}>
                 <Text>G'SOIF</Text>
+                <Text style={styles.text}>G'SOIF is an app about cocktails, browse cocktails check recipe and have fun !</Text>
             </View>
             <View style={styles.sectionContainer}>
                 <Text>Used technologie</Text>
+                <Image style={styles.image} source={react} />
             </View>
             <View style={styles.sectionContainer}>
                 <Text>Our dev team</Text>
+                <Text style={styles.text}>Alexis Toty, Barbara Michaud, Mélyne Leray, Killian Gay</Text>
             </View>
         </SafeAreaView>
     )
--- cocktail_app/src/views/ReadMe/ReadMe.style.js
+++ cocktail_app/src/views/ReadMe/ReadMe.style.js
@@ -5,14 +5,23 @@ const styles = StyleSheet.create({
         backgroundColor: '#1E1E1E',
         flex: 1,
     },
+    sectionContainer: {
+        padding: 20,
+    },
     title: {
         marginVertical: 20,
         fontSize: 40,
         textAlign: 'center',
     },
-    sectionContainer: {
-        padding: 20,
-    }
+    text: {
+        padding: 10,
+    },
+    image: {
+        width: 100,
+        height: 100,
+        marginHorizontal: 10,
+        marginVertical: 20,
+    },
 });
 
 export default styles;
